7993d7ef1de15209fd95bd251dc4a997d66ffc94
Jon Hood <squinky86@gmail.com>
2026-05-13 08:48:19 -0500

    Update for code efficiency and zeroizing

--- upwgen.cpp
+++ upwgen.cpp
@@ -18,12 +18,12 @@
  */
 
 #include <algorithm>
-#include <codecvt>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <locale>
 #include <random>
+#include <vector>
+#include <string>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -31,7 +31,6 @@
 
 using namespace std;
 
-//pulled from https://stackoverflow.com/questions/5698002/how-does-one-securely-clear-stdstring/59179980#59179980
 // Pointer to memset is volatile so that compiler must de-reference
 // the pointer and can't assume that it points to any function in
 // particular (such as memset, which it then might further "optimize")
@@ -40,48 +39,82 @@ typedef void* (*memset_t)(void*, int, size_t);
 static volatile memset_t memset_func = memset;
 
 void cleanse(void* ptr, size_t len) {
-	memset_func(ptr, 0, len);
+	if (ptr) memset_func(ptr, 0, len);
 }
 
-//codecvt destructor is not public in GCC; template out
-template<class I, class E, class S>
-struct codecvt2 : std::codecvt<I, E, S>
-{
-	~codecvt2()
-	{ }
+// Secure allocator that zero-fills memory on deallocation
+template <typename T>
+struct SecureAllocator {
+	using value_type = T;
+	SecureAllocator() noexcept {}
+	template <typename U> SecureAllocator(const SecureAllocator<U>&) noexcept {}
+	T* allocate(std::size_t n) {
+		return static_cast<T*>(std::malloc(n * sizeof(T)));
+	}
+	void deallocate(T* p, std::size_t n) {
+		if (p) {
+			cleanse(p, n * sizeof(T));
+			std::free(p);
+		}
+	}
 };
+
+using SecureU32String = std::basic_string<char32_t, std::char_traits<char32_t>, SecureAllocator<char32_t>>;
+using SecureString = std::basic_string<char, std::char_traits<char>, SecureAllocator<char>>;
+
+void to_utf8(const SecureU32String& input, SecureString& output) {
+	output.clear();
+	for (char32_t codepoint : input) {
+		if (codepoint <= 0x7F) {
+			output += static_cast<char>(codepoint);
+		} else if (codepoint <= 0x7FF) {
+			output += static_cast<char>(0xC0 | (codepoint >> 6));
+			output += static_cast<char>(0x80 | (codepoint & 0x3F));
+		} else if (codepoint <= 0xFFFF) {
+			output += static_cast<char>(0xE0 | (codepoint >> 12));
+			output += static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F));
+			output += static_cast<char>(0x80 | (codepoint & 0x3F));
+		} else if (codepoint <= 0x10FFFF) {
+			output += static_cast<char>(0xF0 | (codepoint >> 18));
+			output += static_cast<char>(0x80 | ((codepoint >> 12) & 0x3F));
+			output += static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F));
+			output += static_cast<char>(0x80 | (codepoint & 0x3F));
+		}
+	}
+}
 
 void PrintHelp()
 {
 	cout << "Unspeakable Password Generator Version 0.0.1." << endl;
-	cout << "Usage: upwgen [-sclnefgh] [password size] [password quantity]" << endl;
-	cout << "\t-s Exclude at least 1 symbol" << endl;
-	cout << "\t-c Exclude at least 1 English capital letter" << endl;
-	cout << "\t-l Exclude at least 1 English lowercase letter" << endl;
-	cout << "\t-d Exclude at least 1 digit/number" << endl;
-	cout << "\t-n Exclude at least 1 invisible character" << endl;
-	cout << "\t-e Exclude at least 1 emoji" << endl;
-	cout << "\t-x Exclude at least 1 extinct language character" << endl;
-	cout << "\t-g Exclude at least 1 gamepiece" << endl;
+	cout << "Usage: upwgen [-scldnexgh] [password size] [password quantity]" << endl;
+	cout << "\t-s Disable symbols" << endl;
+	cout << "\t-c Disable English capital letters" << endl;
+	cout << "\t-l Disable English lowercase letters" << endl;
+	cout << "\t-d Disable digits/numbers" << endl;
+	cout << "\t-n Disable invisible characters" << endl;
+	cout << "\t-e Disable emojis" << endl;
+	cout << "\t-x Disable extinct language characters" << endl;
+	cout << "\t-g Disable gamepieces" << endl;
 	cout << "Default size: 15" << endl;
 	cout << "Default quantity: 1" << endl;
 }
 
 //using the provided engine, require 1 character fom the provided keyspace2
 //If updatekeyspace is true, add the keyspace2 to keyspace.
-void Require(default_random_engine *e, u32string *keyspace, u32string passwords[], int qty, const u32string keyspace2, bool updateKeyspace = true)
+template<typename URBG>
+void Require(URBG &e, SecureU32String *keyspace, vector<SecureU32String> &passwords, int qty, const SecureU32String keyspace2, bool updateKeyspace = true)
 {
 	uniform_int_distribution<int> u(0, keyspace2.length() - 1);
 	for (int i = 0; i < qty; i++)
 	{
-		passwords[i] += keyspace2[u(*e)];
+		passwords[i] += keyspace2[u(e)];
 	}
 	if (updateKeyspace)
 	{
 		*keyspace += keyspace2;
 		sort(begin(*keyspace), end(*keyspace));
 		auto duplicates = unique(begin(*keyspace), end(*keyspace));
-		*keyspace = u32string(begin(*keyspace), duplicates);
+		keyspace->erase(duplicates, end(*keyspace));
 	}
 }
 
@@ -98,11 +131,10 @@ int main(int argc, char *argv[])
 	bool requireGame = true;
 
 	//collection of possible characters to randomly choose from
-	u32string keyspace;
+	SecureU32String keyspace;
 
 	//random number generator
-	random_device r;
-	default_random_engine e(r());
+	random_device e;
 
 	int size = -1;
 	int qty = -1;
@@ -143,79 +175,75 @@ int main(int argc, char *argv[])
 	}
 
 	//set sane defaults
-	if (size < 1)
+	if (size < 0)
 		size = 15;
-	if (qty < 1)
+	if (qty < 0)
 		qty = 1;
 
-	bool err = false;
 	if (size == 0)
 	{
 		cerr << "ERROR: invalid size of password string." << endl;
-		err = true;
+		PrintHelp();
+		return EXIT_FAILURE;
 	}
 	if (qty == 0)
 	{
 		cerr << "ERROR: invalid quantity of passwords specified." << endl;
-		err = true;
-	}
-	if (err)
-	{
 		PrintHelp();
 		return EXIT_FAILURE;
 	}
 
-	//declare volatile to zero-out memory at the end
-	u32string passwords[qty];
+	//Use vector instead of VLA
+	vector<SecureU32String> passwords(qty);
 	
 	//require symbols
 	if (requireSymbol)
 	{
-		Require(&e, &keyspace, passwords, qty, U"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+		Require(e, &keyspace, passwords, qty, U"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
 	}
 
 	//require capitalization
 	if (requireCap)
 	{
-		Require(&e, &keyspace, passwords, qty, U"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		Require(e, &keyspace, passwords, qty, U"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	}
 
 	//require lowercase
 	if (requireLower)
 	{
-		Require(&e, &keyspace, passwords, qty, U"abcdefghijklmnopqrstuvwxyz");
+		Require(e, &keyspace, passwords, qty, U"abcdefghijklmnopqrstuvwxyz");
 	}
 
 	//require digit
 	if (requireDigit)
 	{
-		Require(&e, &keyspace, passwords, qty, U"0123456789");
+		Require(e, &keyspace, passwords, qty, U"0123456789");
 	}
 
 	//require nonprintable
 	if (requireNonprint)
 	{
-		u32string tmpKeyspace;
+		SecureU32String tmpKeyspace;
 		for (char32_t i = U'\U000E0020'; i < U'\U000E007F'; i++)
 			tmpKeyspace += i;
 
-		Require(&e, &keyspace, passwords, qty, tmpKeyspace);
+		Require(e, &keyspace, passwords, qty, tmpKeyspace);
 	}
 
 	//require emoji
 	if (requireEmoji)
 	{
-		u32string tmpKeyspace;
+		SecureU32String tmpKeyspace;
 		for (char32_t i = U'\U0001F600'; i <= U'\U0001F64F'; i++)
 			tmpKeyspace += i;
 
-		Require(&e, &keyspace, passwords, qty, tmpKeyspace);
+		Require(e, &keyspace, passwords, qty, tmpKeyspace);
 	}
 
 	//require an extinct language
 	if (requireExtinct)
 	{
-		u32string tmpKeyspace;
+		SecureU32String tmpKeyspace;
 		//choose a random foreign language
 		uniform_int_distribution<int> u(0, 7);
 		switch (u(e))
@@ -282,15 +310,15 @@ int main(int argc, char *argv[])
 				break;
 		}
 
-		Require(&e, &keyspace, passwords, qty, tmpKeyspace);
+		Require(e, &keyspace, passwords, qty, tmpKeyspace);
 	}
 
 	//require a gamepiece
 	if (requireGame)
 	{
-		u32string tmpKeyspace;
+		SecureU32String tmpKeyspace;
 		//chess pieces
-		for (char32_t i = u'\u2654'; i <= u'\u265F'; i++)
+		for (char32_t i = U'\u2654'; i <= U'\u265F'; i++)
 			tmpKeyspace += i;
 
 		//chess symbols
@@ -319,11 +347,17 @@ int main(int argc, char *argv[])
 		for (char32_t i = U'\U0001FA60'; i <= U'\U0001FA6D'; i++)
 			tmpKeyspace += i;
 
-		Require(&e, &keyspace, passwords, qty, tmpKeyspace);
+		Require(e, &keyspace, passwords, qty, tmpKeyspace);
+	}
+
+	if (keyspace.empty())
+	{
+		cerr << "ERROR: All character sets disabled. No password can be generated." << endl;
+		return EXIT_FAILURE;
 	}
 
 	while (passwords[0].length() < static_cast<unsigned long>(size))
-		Require(&e, &keyspace, passwords, qty, keyspace, false);
+		Require(e, &keyspace, passwords, qty, keyspace, false);
 
 	//shuffle passwords to randomize character order
 	for (int i = 0; i < qty; i++)
@@ -332,18 +366,15 @@ int main(int argc, char *argv[])
 	}
 
 	//print passwords
-	wstring_convert<codecvt2<char32_t, char, std::mbstate_t>, char32_t> conv32;
 #ifdef _WIN32
 	SetConsoleOutputCP(CP_UTF8);
 	setvbuf(stdout, nullptr, _IOFBF, 1000);
 #endif
 	for (int i = 0; i < qty; i++)
 	{
-		cout << i+1 << ": |" << conv32.to_bytes(passwords[i]) << "|" << endl;
-		//zeroize password
-		passwords[i].resize(passwords[i].capacity(), 0);
-		cleanse(&passwords[i][0], passwords[i].size());
-		passwords[i].clear();
+		SecureString utf8_pw;
+		to_utf8(passwords[i], utf8_pw);
+		cout << i+1 << ": |" << utf8_pw << "|" << endl;
 	}
 
 	return EXIT_SUCCESS;
